6bb67b0e4e5948754e37d9bf5c4e5ba6f1c81cbd
A1ex23 <93762189+A1ex23@users.noreply.github.com>
2022-03-25 22:03:03 +0800

    Add files via upload

---
+++ NumSort.java
@@ -0,0 +1,32 @@
+public class NumSort {
+    public static void main(String[] args) {
+        int[]a = new int[args.length];
+        for (int i=0;i<args.length;i++){
+            a[i] = Integer.parseInt(args[i]);
+        }
+        print(a);
+        selectionSort(a);
+        print(a);
+    }
+    //选择排序
+
+    private static void selectionSort(int[] a ){
+        for(int i =0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if (a[j]<a[i]){
+                    int temp =a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+    }
+
+    private static void print(int[] a){
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i] +"");
+
+        }
+        System.out.println();
+    }
+}
